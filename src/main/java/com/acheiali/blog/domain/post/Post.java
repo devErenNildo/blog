991b0c352df53f2663d23d271eb729cebd042b12
devErenNildo <dev.erenildo@gmail.com>
2024-12-21 16:54:41 -0400

    criando classe inicail Post

---
+++ src/main/java/com/acheiali/blog/domain/post/Post.java
@@ -0,0 +1,10 @@
+package com.acheiali.blog.domain.post;
+
+import java.util.Date;
+
+public class Post {
+    private Long postId;
+    private String titulo;
+    private String conteudo;
+    private Date dataCriacao;
+}
---
+++ src/test/java/com/acheiali/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acheiali.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
